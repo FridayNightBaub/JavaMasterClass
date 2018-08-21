373350f4af1a97345e64e19ae1f7bdd4a7118d0e
Bobby Fenn <bfenn138@gmail.com>
2018-08-20 21:20:50 -0600

    HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    public static void main(String[] args){
+
+        System.out.println("Hello, World!");
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+        int newTotal = 1000 - myTotal;
+
+        System.out.println(newTotal);
+
+    }
+}
+
+
